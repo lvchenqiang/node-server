710c8053204636e53ec5272cb79d57bc13f9dff3
吕陈强 <lvchenqiang@souche.com>
2019-11-11 09:51:09 +0800

    add post 80

--- bin/www.js
+++ bin/www.js
@@ -1,10 +1,10 @@
 const http = require('http')
 
-const PORT = 9090
+const PORT = 80
 const serverHandle = require('../app')
 
 const server = http.createServer(serverHandle)
 
 server.listen(PORT,'0.0.0.0',function() {
-    console.log('Web服务器开始监听9090端口')
+    console.log('Web服务器开始监听80端口')
 })
